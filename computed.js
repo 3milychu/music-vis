3ab300d0ab0c47b0d180546423af80027cc6e9d5
3milychu <3milychu@gmail.com>
2019-04-01 01:43:04 -0400

    update syntax for creating comments, add viewport

---
+++ computed.js
@@ -0,0 +1,16 @@
+// formatTime = d3.format(",.1f");
+// music = document.querySelector('audio');
+
+// var app = new Vue({
+//   el: '#reactions',
+//   data: {
+//   },
+// 	computed: {
+// 	  musicTime: function () {
+// 	   var vue_time = formatTime(music.currentTime);
+// 	   return vue_time;
+// 	  }
+// 	}
+// })
+
+// console.log(app.musicTime);
